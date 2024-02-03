a09d26aa77e79fd31efb97e7f1d0e9a7368b9ea8
Felipe Dias <dias20felipe@gmail.com>
2024-02-03 06:26:37 -0300

    alteracao quiz

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+function quiz  (){
+     
+    let respostaA = prompt("Qual o maior rio do mundo ???")  
+    let resposta1 = "Rio Amazonanas"
+
+    if (respostaA == "Rio são Francisco"){
+        console.log("Resposta correta ")
+    }else{
+        console.log("Resposta Errada")
+    }
+}
+
+quiz()
