dee2b730da5478086678b58acbf1cf741a01411a
Felipe Dias <dias20felipe@gmail.com>
2024-02-08 17:29:21 -0300

    ok

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
+ 
+ 
+ 
  RespostasCorretas = ["Rio São Francisco", "Santos Dumont", "Azul", "Tropical"]
-
 
 function quiz  (){
      
     let respostaA = prompt("Qual o maior rio do mundo ???")  
-    let resposta1 = "Rio Amazonanas"
+    let resposta1 = "Rio Amazonanas 3"
 
     if (respostaA == "Rio são Francisco"){
         console.log("Resposta correta ")
