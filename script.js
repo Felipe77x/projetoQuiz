bb742f15091b86fbc5b513587ad32a3ec0a01776
Felipe Dias <dias20felipe@gmail.com>
2024-02-14 14:29:18 -0300

    ok

--- script.js
+++ script.js
@@ -1,4 +1,68 @@
 
  
  
- 
+ const quizData = [
+    {
+        question: "What is the capital of France?",
+        options: ["Paris", "Rome", "Berlin", "Madrid"],
+        answer: "Paris"
+    },
+    {
+        question: "What is the largest planet in our solar system?",
+        options: ["Earth", "Mars", "Jupiter", "Saturn"],
+        answer: "Jupiter"
+    },
+    {
+        question: "What is 2 + 2?",
+        options: ["3", "4", "5", "6"],
+        answer: "4"
+    }
+];
+
+let currentQuestion = 0;
+let score = 0;
+
+const questionElement = document.getElementById('question');
+const optionsElement = document.getElementById('options');
+const resultElement = document.getElementById('result');
+
+function loadQuestion() {
+    const currentQuizData = quizData[currentQuestion];
+    questionElement.innerText = currentQuizData.question;
+    optionsElement.innerHTML = "";
+    currentQuizData.options.forEach(option => {
+        const button = document.createElement('button');
+        button.innerText = option;
+        button.addEventListener('click', () => checkAnswer(option));
+        optionsElement.appendChild(button);
+    });
+}
+
+function checkAnswer(answer) {
+    const currentQuizData = quizData[currentQuestion];
+    if (answer === currentQuizData.answer) {
+        score++;
+        resultElement.innerText = "Correct!";
+    } else {
+        resultElement.innerText = "Wrong!";
+    }
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    if (currentQuestion < quizData.length) {
+        loadQuestion();
+        resultElement.innerText = "";
+    } else {
+        showResult();
+    }
+}
+
+function showResult() {
+    questionElement.innerText = "";
+    optionsElement.innerHTML = "";
+    resultElement.innerText = `You scored ${score} out of ${quizData.length}`;
+}
+
+loadQuestion();
+
