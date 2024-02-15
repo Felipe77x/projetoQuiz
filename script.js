9518c0cbbb548bdfea94511d3a0a140bc79758d7
Debora Lais <deboralaisnm@gmail.com>
2024-02-15 18:48:28 -0300

    Changes

--- script.js
+++ script.js
@@ -1,93 +1,192 @@
-import questions from './questions.js';
-document.addEventListener('DOMContentLoaded', () => {
-  const questionElement = document.querySelector('.question');
-  const answersElement = document.querySelector('.answers');
-  const questionCountElement = document.querySelector('.spnQtd');
-  const finishMessageElement = document.querySelector('.finish span');
-  const contentElement = document.querySelector('.content');
-  const finishContentElement = document.querySelector('.finish');
-  const restartButton = document.querySelector('#restartBtn');
-  const playButton = document.querySelector('#playBtn'); // Seleciona o botão de jogar
+let titulo = document.querySelector('h1')
+let instrucoes = document.querySelector('#instrucoes')
+let aviso = document.querySelector('#aviso')
+//let respostaEsta = document.querySelector('#respostaEsta')
+let pontos = 0 // pontos para o placar
+let placar = 0 // placar
 
-  let currentIndex =   0;
-  let questionsCorrect =   0;
+// PERGUNTA
+let numQuestao = document.querySelector('#numQuestao')
+let pergunta   = document.querySelector('#pergunta')
 
-  // Função para iniciar o jogo ou reiniciá-lo
-  const startOrRestartGame = () => {
-    contentElement.style.display = "flex";
-    finishContentElement.style.display = "none";
-    playButton.style.display = "none"; // Esconde o botão de jogar quando o jogo começa
+// ALTERNATIVAS
+let a = document.querySelector('#a')
+let b = document.querySelector('#b')
+let c = document.querySelector('#c')
 
-    currentIndex =   0;
-    questionsCorrect =   0;
-    loadQuestion();
-  };
+// article com a class questoes
+let articleQuestoes = document.querySelector('.questoes')
+// ol li com as alternativas
+let alternativas = document.querySelector('#alternativas')
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const startOrRestartGame = document.querySelector('#playBtn'); // Substitua #playButton pelo seletor correto para o seu botão
-    startOrRestartGame.addEventListener('click', startOrRestartGame);
+const q0 = {
+    numQuestao   : 0,
+    pergunta     : "Pergunta",
+    alternativaA : "Alternativa A",
+    alternativaB : "Alternativa B",
+    alternativaC : "Alternativa C",
+    correta      : "0",
+}
 
-  });
-  
-  // Função para reiniciar o jogo quando o jogador perde
-  const restartGame = () => {
-    contentElement.style.display = "flex";
-    finishContentElement.style.display = "none";
-    playButton.style.display = "none"; // Esconde o botão de jogar quando o jogo é reiniciado
+const q1 = {
+    numQuestao   : 1,
+    pergunta     : "Boreal é o mesmo que...",
+    alternativaA : "Sul",
+    alternativaB : "Leste",
+    alternativaC : "Norte",
+    correta      : "Norte",
+}
 
-    currentIndex =   0;
-    questionsCorrect =   0;
-    loadQuestion();
-  };
+const q2 = {
+    numQuestao   : 2,
+    pergunta     : "Austral é o mesmo que...",
+    alternativaA : "Oeste",
+    alternativaB : "Sul",
+    alternativaC : "Norte",
+    correta      : "Sul",
+}
 
-  restartButton.addEventListener('click', restartGame);
-  playButton.addEventListener('click', startOrRestartGame); // Adiciona um listener para o botão de jogar
+const q3 = {
+    numQuestao   : 3,
+    pergunta     : "Nascente é o mesmo que...",
+    alternativaA : "Sul",
+    alternativaB : "Leste",
+    alternativaC : "Oeste",
+    correta      : "Leste",
+}
 
-  const nextQuestionHandler = (e) => {
-    if (e.target.getAttribute("data-correct") === "true") {
-      questionsCorrect++;
-    }
+const q4 = {
+    numQuestao   : 4,
+    pergunta     : "Poente é o mesmo que...",
+    alternativaA : "Norte",
+    alternativaB : "Leste",
+    alternativaC : "Oeste",
+    correta      : "Oeste",
+}
 
-    if (currentIndex < questions.length -   1) {
-      currentIndex++;
-      loadQuestion();
+const q5 = {
+    numQuestao   : 5,
+    pergunta     : "O Brasil se localiza em qual continente?",
+    alternativaA : "África",
+    alternativaB : "Europa",
+    alternativaC : "América",
+    correta      : "América",
+}
+
+// CONSTANTE COM UM ARRAY DE OBJETOS COM TODAS AS QUESTOES
+const questoes = [q0, q1, q2, q3, q4, q5]
+
+let numero = document.querySelector('#numero')
+let total  = document.querySelector('#total')
+
+numero.textContent = q1.numQuestao
+
+let totalDeQuestoes = (questoes.length)-1
+console.log("Total de questões " + totalDeQuestoes)
+total.textContent = totalDeQuestoes
+
+// MONTAR A 1a QUESTAO COMPLETA, para iniciar o Quiz
+numQuestao.textContent = q1.numQuestao
+pergunta.textContent   = q1.pergunta
+a.textContent = q1.alternativaA
+b.textContent = q1.alternativaB
+c.textContent = q1.alternativaC
+
+// CONFIGURAR O VALUE INICIAL DA 1a QUESTAO COMPLETA
+a.setAttribute('value', '1A')
+b.setAttribute('value', '1B')
+c.setAttribute('value', '1C')
+
+// PARA MONTAR AS PROXIMAS QUESTOES
+function proximaQuestao(nQuestao) {
+    numero.textContent = nQuestao
+    numQuestao.textContent = questoes[nQuestao].numQuestao
+    pergunta.textContent   = questoes[nQuestao].pergunta
+    a.textContent = questoes[nQuestao].alternativaA
+    b.textContent = questoes[nQuestao].alternativaB
+    c.textContent = questoes[nQuestao].alternativaC
+    a.setAttribute('value', nQuestao+'A')
+    b.setAttribute('value', nQuestao+'B')
+    c.setAttribute('value', nQuestao+'C')
+}
+
+function bloquearAlternativas() {
+    a.classList.add('bloqueado')
+    b.classList.add('bloqueado')
+    c.classList.add('bloqueado')
+}
+
+function desbloquearAlternativas() {
+    a.classList.remove('bloqueado')
+    b.classList.remove('bloqueado')
+    c.classList.remove('bloqueado')
+}
+
+function verificarSeAcertou(nQuestao, resposta) {
+
+    let numeroDaQuestao = nQuestao.value
+    console.log("Questão " + numeroDaQuestao)
+
+    let respostaEscolhida = resposta.textContent
+    //console.log("RespU " + respostaEscolhida)
+
+    let certa = questoes[numeroDaQuestao].correta
+    //console.log("RespC " + certa)
+
+    if(respostaEscolhida == certa) {
+        //console.log("Acertou")
+        //respostaEsta.textContent = "Correta 😊"
+        pontos += 10 // pontos = pontos + 10
     } else {
-      finishGame();
+        //console.log("Errou!")
+        //respostaEsta.textContent = "Errada 😢"
     }
-  };
 
-  const finishGame = () => {
-    finishMessageElement.innerHTML = `Você acertou ${questionsCorrect} de ${questions.length}.`;
-    contentElement.style.display = "none";
-    finishContentElement.style.display = "flex";
-    playButton.style.display = "block"; // Mostra o botão de jogar quando o jogo termina
-  };
+    // atualizar placar
+    placar = pontos
+    instrucoes.textContent = "Pontos " + placar
 
-  const loadQuestion = () => {
-    questionCountElement.innerHTML = `${currentIndex +   1}/${questions.length}`;
-    const item = questions[currentIndex];
-    answersElement.innerHTML = "";
-    questionElement.innerHTML = item.question;
+    // bloquear a escolha de opcoes
+    bloquearAlternativas()
 
-    item.answers.forEach((answer) => {
-      const answerDiv = document.createElement("div");
+    setTimeout(function() {
+        //respostaEsta.textContent = '...'
+        proxima = numeroDaQuestao+1
 
-      answerDiv.innerHTML = `
-      <button class="answer" data-correct="${answer.correct}">
-        ${answer.option}
-      </button>
-      `;
+        if(proxima > totalDeQuestoes) {
+            console.log('Fim do Jogo!')
+            fimDoJogo()
+        } else {
+            proximaQuestao(proxima)
+        }
+    }, 250)
+    desbloquearAlternativas()
+}
 
-      answersElement.appendChild(answerDiv);
-    });
+function fimDoJogo() {
+    instrucoes.textContent = "Fim de Jogo!"
+    numQuestao.textContent = ""
 
-    document.querySelectorAll(".answer").forEach((answerItem) => {
-      answerItem.addEventListener("click", nextQuestionHandler);
-    });
-    
-  };
-  
-  // Inicializa o jogo
-  startOrRestartGame();
-  
-});
+    let pont = ''
+    pontos == 0 ? pont = 'ponto' : pont = 'pontos'
+
+    pergunta.textContent   = "Você conseguiu " + pontos + " " + pont
+
+    aviso.textContent = "Você conseguiu " + pontos + " " + pont
+
+    a.textContent = ""
+    b.textContent = ""
+    c.textContent = ""
+
+    a.setAttribute('value', '0')
+    b.setAttribute('value', '0')
+    c.setAttribute('value', '0')
+
+    // OCULTAR O ARTICLE DA QUESTAO
+    articleQuestoes.style.display = 'none'
+
+    setTimeout(function() {
+        pontos = 0 // zerar placar
+        location.reload();
+    }, 2000)
+}
